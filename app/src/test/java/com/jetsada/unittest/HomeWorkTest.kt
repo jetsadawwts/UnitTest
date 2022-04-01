801cfb210719c3910c4f6a2e950dce5d28559a30
jetsadawwts <jetsadawwts@gmail.com>
2022-04-01 14:25:47 +0700

    Unit Test On Kotlin

---
+++ app/src/test/java/com/jetsada/unittest/HomeWorkTest.kt
@@ -0,0 +1,9 @@
+package com.jetsada.unittest
+
+import org.junit.Test
+import com.google.common.truth.Truth.assertThat
+
+
+class HomeWorkUnitTest {
+
+}
---
+++ app/src/test/java/com/jetsada/unittest/RegistrationUtilTest.kt
@@ -0,0 +1,69 @@
+package com.jetsada.unittest
+
+import org.junit.Test
+import com.google.common.truth.Truth.assertThat
+
+class RegistrationUtilTest {
+
+    @Test
+    fun `empty username return false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "",
+            "123",
+        "123"
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `valid username and correctly repeated password return true`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Jetsada",
+            "123",
+            "123"
+        )
+        assertThat(result).isTrue()
+
+    }
+
+    @Test
+    fun `username already exists return false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Carl",
+            "123",
+            "123"
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `incorrectly confirmed password returns false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Jetsada",
+            "123456",
+            "abcdefg"
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `empty password returns false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Jetsada",
+            "",
+            ""
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `less than 2 digit password returns false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Jetsada",
+            "abcdefg5",
+            "abcdefg5"
+        )
+        assertThat(result).isFalse()
+    }
+
+}
